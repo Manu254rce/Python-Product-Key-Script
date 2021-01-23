b7964783a7b783f7bfddd67950e1a40e62a51717
Manu254rce <69007849+Manu254rce@users.noreply.github.com>
2021-01-23 21:41:26 +0300

    Update productActivation.py
    It's an unbelievably short code, since it only covers a single command line that extracts the Windows product key from system registry

--- productActivation.py
+++ productActivation.py
@@ -1,7 +1,12 @@
-import subprocess, re, pyperclip
+#Python Product Activation Key for Windows
+#Written by Manu254rce, all rights reserved
 
-command_line = subprocess.run(["wmic", "path", "softwarelicensingservice", "get", "OA3xOriginalProductKey"], capture_output = True).stdout.decode()
+import subprocess, pyperclip
+
+command_line = subprocess.run(["wmic", "path", "softwarelicensingservice", "get", "OA3xOriginalProductKey"], capture_output = True).stdout.decode()#run the code to open up cmd interface
+#and key in the strings
 print(command_line)
+#the program will open up the command line, print the output in the python shell, then pyperclip will copy the code onto the clipboard
 
 pyperclip.copy(command_line)
 
